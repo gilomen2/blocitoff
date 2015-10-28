1e400ac5a7183098bab59653c1a829720e057d8c
Beth Gilomen <gilomen2@gmail.com>
2015-10-28 17:13:33 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.new(
-  name: 'Alice',
-  email: 'alice@nowhere.com',
-  password: 'password'
+beth = User.new(
+  name:     'Beth',
+  email:    'beth@example.com',
+  password: 'helloworld'
 )
-user.skip_confirmation!
-user.save!
+beth.skip_confirmation!
+beth.save!
+
+ben = User.new(
+  name:     'Ben',
+  email:    'ben@example.com',
+  password: 'helloworld'
+)
+ben.skip_confirmation!
+ben.save!
+
+kevin = User.new(
+  name:     'Kevin',
+  email:    'kevin@example.com',
+  password: 'bbbunnyboy'
+)
+kevin.skip_confirmation!
+kevin.save!
+
+users = User.all
+
+35.times do
+  Item.create!(
+    name: Faker:: Lorem.sentence,
+    user: users.sample
+  )
+end
+
+
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} todos created"
+
